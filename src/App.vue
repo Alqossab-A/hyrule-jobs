b8fd678fc7367531382b1c568e5fbb48f1bd7ab5
Ahmed <alqossaba@gmail.com>
2024-04-29 02:26:01 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ name }} - {{ age }}</p>
+  <button @click="changeName('zelda'), changeAge(33)">change name</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 25 as number | string,
+    }
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: number | string) {
+      this.age = age;
+    }
+  },
+});
+</script>
+
+<style scoped></style>
