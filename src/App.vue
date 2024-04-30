2b1b4bdd7f2f20a929b3a8f0ffa4526e792e5b81
Ahmed <alqossaba@gmail.com>
2024-04-30 01:53:30 -0400

    (feat):JobList component with fake data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
-  <p>{{ name }} - {{ age }}</p>
-  <button @click="changeName('zelda'), changeAge(33)">change name</button>
+  <div>
+    <JobList :jobs="jobs" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { defineComponent, ref } from 'vue'
+import JobList from './components/JobList.vue'
+import type { Job } from './types/Job'
 
 export default defineComponent({
   name: 'App',
-  components: {},
-  data() {
-    return {
-      name: 'Link',
-      age: 25 as number | string,
-    }
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-    }
-  },
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'farm worker', location: 'riverdale ranch', salary: 30000, id: 1 },
+      { title: 'warehouse packer', location: 'amazon', salary: 40000, id: 2 },
+      { title: 'plumber', location: 'rocket toilets', salary: 50000, id: 3 },
+      { title: 'driver', location: 'lift', salary: 25000, id: 4 },
+      { title: 'fisherman', location: 'riverdale ranch', salary: 23000, id: 5 },
+      { title: 'project manager', location: 'officair', salary: 70000, id: 6 },
+    ])
+
+    return { jobs }
+  }
 });
 </script>
 
---
+++ src/components/JobList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} at {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} gold</p>
+        </div>
+        <div class="description">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum cum inventore architecto corporis,
+            quaerat nobis est corrupti, tenetur quis ducimus qui name totam fuga. Vel totam laudantium veniam rerum
+            esse.
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+import type { Job } from '@/types/Job';
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>
+    }
+  }
+})
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+
+.job-list ul {
+  padding: 0;
+}
+
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+
+.salary {
+  display: flex;
+}
+
+.salary img {
+  width: 30px;
+}
+
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
